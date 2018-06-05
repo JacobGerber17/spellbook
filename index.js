0fc960d665f13a9895c83338fa3ef4d7cf48a425
Jacob Gerber <jacogerb@iu.edu>
2018-06-05 14:26:45 -0400

    finish HW 2 and all extra credit, and remove useless code from HW 1

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-const button = document.querySelector('button')
-const header = document.querySelector('h1')
-const header2 = document.querySelector('#header2')
 const form = document.querySelector('form')
-
-const changeHeader = function(){
-    header.innerHTML = "The text has been changed."
-}
-
-const changeHeader2 = function(){
-    header2.innerHTML = "The text has been changed."
-}
 
 const formChange = function(ev){
     ev.preventDefault()
     const f = ev.target
     const formText = f.formText.value
     const manaCost = f.manaCost.value
+    addListItem(formText, manaCost)
+    f.reset()
+}
+
+const addListItem = function (name, cost){
     const spellList = document.querySelector('#spells')
     let spell = document.createElement('li')
-    let spellText = document.createElement('span')
-    spellText.className = 'spellName'
-    let spellCost = document.createElement('span')
-    spellCost.className = 'spellCost'
-    let name = document.createTextNode(formText)
-    let cost = document.createTextNode('(' + manaCost + 'MP)')
-    spellText.appendChild(name)
-    spellCost.appendChild(cost)
+    let spellText = addName(name)
+    let spellCost = addCost(cost)
     spell.appendChild(spellText)
     spell.appendChild(spellCost)
     spellList.appendChild(spell)
-    f.reset()
-    
 }
 
+const addName = function(name){
+    let spellText = document.createElement('span')
+    spellText.className = 'spellName'
+    let spellName = document.createTextNode(name)
+    spellText.appendChild(spellName)
+    return spellText
+}
 
-//button.addEventListener('click', changeHeader2)
+const addCost = function(cost){
+    let spellCost = document.createElement('span')
+    spellCost.className = 'spellCost'
+    let mp = document.createTextNode('(' + cost + 'MP)')
+    spellCost.appendChild(mp)
+    return spellCost
+}
+
 form.addEventListener('submit', formChange)
