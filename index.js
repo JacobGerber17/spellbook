74467ba572c1e5e8447366874c084ac38ac0b6d4
Jacob Gerber <jacogerb@iu.edu>
2018-06-06 12:04:42 -0400

    make app object that contains all functions used

--- index.js
+++ index.js
@@ -1,41 +1,48 @@
-const form = document.querySelector('form')
+const app = {
+    init: function(){
+        const form = document.querySelector('form')
+        form.addEventListener('submit', ev => {
+            this.handleSubmit(ev)
+        })
+    },
 
-const handleSubmit = function(ev){
-    ev.preventDefault()
-    const f = ev.target
-    const spell = {
-        name: f.spellName.value,
-        cost: f.manaCost.value,
+    handleSubmit: function(ev) {
+        ev.preventDefault()
+        const f = ev.target
+        const spell = {
+            name: f.spellName.value,
+            cost: f.manaCost.value,
+        }
+        this.addListItem(spell)
+        f.reset()
+    },
+
+    addListItem: function(spellObj){
+        const spellList = document.querySelector('#spells')
+        let spell = document.createElement('li')
+        spell.classList.add('spell')
+        let spellText = this.addName(spellObj.name)
+        let spellCost = this.addCost(spellObj.cost)
+        spell.appendChild(spellText)
+        spell.appendChild(spellCost)
+        spellList.appendChild(spell)
+    },
+
+    addName: function(name) {
+        let spellText = document.createElement('span')
+        spellText.classList.add('spellName')
+        let spellName = document.createTextNode(name)
+        spellText.appendChild(spellName)
+        return spellText
+    },
+
+    addCost: function(cost) {
+        let spellCost = document.createElement('span')
+        spellCost.classList.add('spellCost')
+        let mp = document.createTextNode('(' + cost + 'MP)')
+        spellCost.appendChild(mp)
+        return spellCost
     }
-    addListItem(spell)
-    f.reset()
 }
 
-const addListItem = function (spellObj){
-    const spellList = document.querySelector('#spells')
-    let spell = document.createElement('li')
-    spell.classList.add('spell')
-    let spellText = addName(spellObj.name)
-    let spellCost = addCost(spellObj.cost)
-    spell.appendChild(spellText)
-    spell.appendChild(spellCost)
-    spellList.appendChild(spell)
-}
-
-const addName = function(name){
-    let spellText = document.createElement('span')
-    spellText.classList.add('spellName')
-    let spellName = document.createTextNode(name)
-    spellText.appendChild(spellName)
-    return spellText
-}
-
-const addCost = function(cost){
-    let spellCost = document.createElement('span')
-    spellCost.classList.add('spellCost')
-    let mp = document.createTextNode('(' + cost + 'MP)')
-    spellCost.appendChild(mp)
-    return spellCost
-}
-
-form.addEventListener('submit', handleSubmit)
+app.init()
