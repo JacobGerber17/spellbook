f91d65bb0fd2fd459b431f3982803b4d4797385f
Jacob Gerber <jacogerb@iu.edu>
2018-06-07 10:44:40 -0400

    add template and working favorite button

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const app = {
     init: function(){
+        this.spellArray = []
+        this.template = document.querySelector('.spell.template')
         const form = document.querySelector('form')
         form.addEventListener('submit', ev => {
             this.handleSubmit(ev)
         })
     },
-
-    spellArray: [],
 
     handleSubmit: function(ev) {
         ev.preventDefault()
@@ -14,62 +14,48 @@ const app = {
         const spell = {
             name: f.spellName.value,
             cost: f.manaCost.value,
+            fav: false,
         }
         this.spellArray.push(spell)
-        this.addListItem(spell)
+        const item = this.addListItem(spell)
+        const list = document.querySelector('#spells')
+        list.appendChild(item)
         console.log(this.spellArray)
         f.reset()
+        f.spellName.focus()
     },
 
     addListItem: function(spellObj){
-        const spellList = document.querySelector('#spells')
-        let spell = document.createElement('li')
-        spell.classList.add('spell')
-        let spellText = this.addName(spellObj.name)
-        let spellCost = this.addCost(spellObj.cost)
-        let removeButton = this.addRemoveButton()
-        let editButton = this.addEditButton()
-        spell.appendChild(spellText)
-        spell.appendChild(spellCost)
-        spell.appendChild(editButton)
-        spell.appendChild(removeButton)
-        spellList.appendChild(spell)
-    },
+        const item = this.template.cloneNode(true)
+        item.classList.remove('template')
 
-    addName: function(name) {
-        let spellText = document.createElement('span')
-        spellText.classList.add('spellName')
-        let spellName = document.createTextNode(name)
-        spellText.appendChild(spellName)
-        return spellText
-    },
+        properties = Object.keys(spellObj)
 
-    addCost: function(cost) {
-        let spellCost = document.createElement('span')
-        spellCost.classList.add('spellCost')
-        let mp = document.createTextNode('(' + cost + ' MP)')
-        spellCost.appendChild(mp)
-        return spellCost
-    },
+        properties.forEach(property => {
+            const el = item.querySelector(`.${property}`)
+            if (el) {
+                el.textContent = spellObj[property]
+                el.setAttribute('title', spellObj[property])
+            }
+        })
 
-    addRemoveButton: function() {
-        let removeButton = document.createElement('span')
-        removeButton.textContent = 'X'
-        removeButton.classList.add('delete')
+        const removeButton = item.querySelector('.delete')
         removeButton.addEventListener('click', (ev) =>{
             this.removeSpell(ev)
         })
-        return removeButton;
-    },
 
-    addEditButton: function() {
-        let editButton = document.createElement('span')
-        editButton.textContent = 'Edit'
-        editButton.classList.add('edit')
+        const editButton = item.querySelector('.edit')
         editButton.addEventListener('click', (ev) => {
             this.editSpell(ev)
         })
-        return editButton
+
+        const favButton = item.querySelector('.favButton')
+        favButton.addEventListener('click', (ev) => {
+            this.toggleFavorite(ev)
+        })
+
+        return item
+
     },
 
     removeSpell: function(ev){
@@ -87,19 +73,22 @@ const app = {
     editSpell: function(ev){
         let spell = ev.target.parentNode
         let mySpell = {
-            name: spell.childNodes[0].textContent,
-            cost: spell.childNodes[1].textContent
+            name: spell.childNodes[1].textContent,
+            cost: spell.childNodes[3].textContent
         }
-        let cost = mySpell.cost.split(' ')[0]
-        let finalCost = cost.substring(1, cost.length)
         const form = document.querySelector('form')
+        console.log(mySpell)
         form.spellName.value = mySpell.name
-        form.manaCost.value = finalCost
+        form.manaCost.value = mySpell.cost
         spell.parentNode.removeChild(spell)
-        
         let i = this.spellArray.indexOf(mySpell)
         this.spellArray.splice(i, 1)
         console.log(this.spellArray)
+    },
+
+    toggleFavorite : function(ev){
+        spell = ev.target.parentNode
+        this.spellArray.fav = spell.classList.toggle('favSpell')
     },
 }
 
